a603d191b7c5150f3ed4c2c20a2cd5a07b63e51b
denisrennes <degau35@gmail.com>
2025-11-24 18:36:34 +0100

    Fixed Get_Hash_Duplicates.ps1: now uses [HashSet] instead of Compare-Object  Changes to be committed: 	modified:   Get_Hash_Duplicates.ps1

--- Get_Hash_Duplicates.ps1
+++ Get_Hash_Duplicates.ps1
@@ -45,8 +45,8 @@ $Dir1 = $file_o.FullName
 
 
 # Get [HashInfo] list for $Dir1
-[List[HashInfo]]$files1 = @( Get_Directory_Photo_Hash_Export $Dir1 -Recurse:${Recurse} )
-Out normal " '$Dir1': $($files1.Count) computed hash"
+[List[HashInfo]]$dir1_hashinfo_list = @( Get_Directory_Photo_Hash_Export $Dir1 -Recurse:${Recurse} )
+Out normal " '$Dir1': $($dir1_hashinfo_list.Count) computed hash"
 
 if ( $Dir2 ) {
 
@@ -58,26 +58,28 @@ if ( $Dir2 ) {
     $Dir2 = $file_o.FullName
 
     # Get [HashInfo] list for $Dir2
-    [List[HashInfo]]$files2 = @( Get_Directory_Photo_Hash_Export $Dir2 -Recurse:${Recurse} )
-    Out normal " '$Dir2': $($files2.Count) computed hash"
+    [List[HashInfo]]$dir2_hashinfo_list = @( Get_Directory_Photo_Hash_Export $Dir2 -Recurse:${Recurse} )
+    Out normal " '$Dir2': $($dir2_hashinfo_list.Count) computed hash"
 
-    # Compare $Dir2 and $Dir1 
-    Out normal "Computing files having the same hash in both directories..."
-    $file2_same_hash_list = @( compare-object $files2  $files1 -Property Hash -ExcludeDifferent -IncludeEqual -Passthru)
-    Out normal "`$file2_same_hash_list = identical Hash: $($file2_same_hash_list.Count)"
+    # Get all files from $Dir2 having a Hash already existing in $Dir1 (Compare-Object does not work here because the same hash value appears multiple times.)
+    Out normal -NoNewLine "Searching files from `"${Dir2}`" having a Hash existing in `"${Dir1}`"... " -Highlight_Text $Dir2
+    $dir1_hash_hashset = [HashSet[string]]::new()
+    $dir1_hashinfo_list.Hash | ForEach-Object { $null = $dir1_hash_hashset.Add($_) }
+    [List[HashInfo]]$dir2_same_hash_hashinfo_list = @( $dir2_hashinfo_list | Where-Object { $dir1_hash_hashset.Contains($_.Hash) } )
+    Out normal ": $($dir2_same_hash_hashinfo_list.Count)"
 
     # Delete files in $Dir2 having the same hash than a file in $Dir1?
-    if ( $file2_same_hash_list.Count -ne 0 ) {
+    if ( $dir2_same_hash_hashinfo_list.Count -ne 0 ) {
 
         # Display the files from $Dir2 having the same hash than a file in $Dir1
         Out normal ''
         Out normal "Files from `"${Dir2}`" having the same hash than a file in `"${Dir1}`":"
-        $file2_same_hash_list.RelativePath | Out normal
+        $dir2_same_hash_hashinfo_list.RelativePath | Out normal
         
         # User input: Confirm deleting files in $Dir2 having the same hash than a file in $Dir1?
         $input_default = 'n'
         Do {
-            Out normal -NoNewLine "Confirm to delete the above files from `"${Dir2}`" having the same hash than a file in `"${Dir1}`" [y/n(default)]: " -Highlight_Text $Dir2
+            Out normal -NoNewLine "Confirm to delete the above $($dir2_same_hash_hashinfo_list.Count) files from `"${Dir2}`" having the same hash than a file in `"${Dir1}`" [y/n(default)]: " -Highlight_Text $Dir2
             $user_input = Read-Host
             if (-not $user_input ) { $user_input = $input_default }
         } Until ( $user_input -in ('y','n') ) 
@@ -87,7 +89,7 @@ if ( $Dir2 ) {
         }
 
         # Delete the files from Dir2
-        $file2_same_hash_list.RelativePath | ForEach-Object {
+        $dir2_same_hash_hashinfo_list.RelativePath | ForEach-Object {
             $file_fullname = Join-Path $Dir2 $_
             Out normal -NoNewLine "Deleting `"${file_fullname}`"... " -Highlight_Text ${file_fullname}
             Remove-Item $file_fullname -ErrorAction Stop
